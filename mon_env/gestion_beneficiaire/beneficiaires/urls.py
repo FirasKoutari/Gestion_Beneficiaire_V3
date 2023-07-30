3fa88e7c826bb7a45a0aece204379eaf42a3a864
FirasKoutari <134013450+FirasKoutari@users.noreply.github.com>
2023-07-30 17:58:01 +0200

    modificatio of view

--- mon_env/gestion_beneficiaire/beneficiaires/urls.py
+++ mon_env/gestion_beneficiaire/beneficiaires/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 urlpatterns = [
-     path('gestion_bn/', views.gestion_bn, name='gestion_bn'),
+    path('gestion_bn/', views.gestion_bn, name='gestion_bn'),
     path('add_beneficiary/', views.add_beneficiary, name='add_beneficiary'),
     path('update_beneficiary/<str:cin>/', views.update_beneficiary, name='update_beneficiary'),
     path('delete_beneficiary/<str:cin>/', views.delete_beneficiary, name='delete_beneficiary'),
